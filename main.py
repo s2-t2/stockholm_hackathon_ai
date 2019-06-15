646efdbe1cfa11fcaa73792ec3bb69fbe9370f21
Sujata <sujata.tamang@protonmail.ch>
2019-06-15 16:02:28 +0200

    spooky author

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ csv = pandas.read_csv("data/spooky-author-identification/train.csv")
 data = list(csv.text)
 target = list(csv.author)
 
+testcsv = pandas.read_csv("data/spooky-author-identification/test.csv")
+testdata = list(testcsv.text)
+testid = list(testcsv.id)
+
 pipeline = Pipeline([
     ('vect', CountVectorizer()),
     ('tfidf', TfidfTransformer()),
@@ -24,15 +28,16 @@ pipeline = Pipeline([
 # uncommenting more parameters will give better exploring power but will
 # increase processing time in a combinatorial way
 parameters = {
-    'vect__max_df': (0.5, 0.75, 1.0),
-    # 'vect__max_features': (None, 5000, 10000, 50000),
-    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+    'vect__max_df': (0.75,),
+    #'vect__max_features': (None, 5000, 10000, 50000),
+    'vect__ngram_range': ((1, 5),),  # unigrams or bigrams
     # 'tfidf__use_idf': (True, False),
-    # 'tfidf__norm': ('l1', 'l2'),
-    'clf__max_iter': (20,),
-    'clf__alpha': (0.00001, 0.000001),
-    'clf__penalty': ('l2', 'elasticnet'),
-    # 'clf__max_iter': (10, 50, 80),
+    #'tfidf__norm': ('l1', 'l2'),
+    #'clf__max_iter': (20,),
+    'clf__alpha': (1e-7),
+    #'clf__penalty': ('l2', 'elasticnet'),
+    'clf__loss': ('log',),
+    'clf__max_iter': (160,),
 }
 
 if __name__ == "__main__":
@@ -55,3 +60,21 @@ if __name__ == "__main__":
     best_parameters = grid_search.best_estimator_.get_params()
     for param_name in sorted(parameters.keys()):
         print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+    output = grid_search.predict_proba(testdata)
+    result = pandas.DataFrame(output, columns=grid_search.classes_)
+
+    id = pandas.DataFrame(testid, columns =["id"])
+    res = id.join(result).set_index('id')
+
+    res.to_csv("result.csv")
+
+
+
+
+    
+
+
+
+
+
