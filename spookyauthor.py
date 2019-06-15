469c780fa8157be0169f19fb72a34fd033aa39d6
Jim Holmstrom <jim.holmstroem@gmail.com>
2019-06-15 11:55:56 +0200

    firetruck

--- spookyauthor.py
+++ spookyauthor.py
@@ -2,6 +2,25 @@
 
 import pandas as pd
 
+from collections import Counter
+
 data = pd.read_csv("data/spooky-author-identification/train.csv")
 
-print (data.head())
+authors = data.groupby(by='author').text.apply(" ".join)
+
+result = (
+    authors
+        .str.lower()
+        .str.replace(".", "")
+        .str.replace(",", "")
+        .str.replace("'", "")
+        .str.replace("\"", "")
+        .str.replace(",", "")
+        .str.replace(";", "")
+        .str.replace("!", "")
+        .str.replace("?", "")
+        .str.split()
+        .apply(Counter)
+)
+
+eap = result["EAP"]
